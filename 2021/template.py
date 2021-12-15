62628abfe700d2f17402c80bcf035e923e767614
Kate Yu <katexyu@gmail.com>
2021-12-14 23:22:26 -0800

    add template

---
+++ 2021/template.py
@@ -0,0 +1,28 @@
+from helpers import *
+from collections import *
+from itertools import *
+from math import *
+from heapq import heappush, heappop, heappushpop, heapify, heapreplace
+
+
+example = """
+"""
+
+actual = """
+"""
+
+
+def solve(inp):
+    inp = inp.strip().split('\n')
+
+    ans = ''
+    return ans 
+
+
+if __name__=='__main__':
+    example_ans = solve(example)
+    print(f'example:\n {example_ans}')
+
+    actual_ans = solve(actual)
+    print(f'actual:\n {actual_ans}')
+
