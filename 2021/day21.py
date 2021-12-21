3fef8a633e2f6b34df3fd21ca8fd32c1f5ec7647
Kate Yu <katexyu@gmail.com>
2021-12-20 23:02:18 -0800

    Add day 21

---
+++ 2021/day21.py
@@ -0,0 +1,96 @@
+from helpers import *
+from collections import *
+from itertools import *
+from math import *
+from heapq import heappush, heappop, heappushpop, heapify, heapreplace
+from functools import cache
+
+
+example = [4, 8] 
+
+actual = [2, 5]
+
+
+class Die:
+    def __init__(self):
+        self.turns = 0
+
+    def roll(self):
+        self.turns += 1
+        if self.turns > 100:
+            self.turns = 1
+        return self.turns
+    
+
+def solve(inp):
+    die = Die()
+    p1, p2 = inp
+
+    pts1 = 0
+    pts2 = 0
+    turn = True
+    while pts1 < 1000 and pts2 < 1000:
+        if turn:
+            p1 += die.roll() + die.roll() + die.roll() 
+            p1 = (p1-1) % 10 + 1
+            pts1 += p1
+        else:
+            p2 += die.roll() + die.roll() + die.roll() 
+            p2 = (p2-1) % 10 + 1
+            pts2 += p2
+        turn = not turn
+    loser = min(pts1, pts2)
+    print(loser)
+    print(die.turns)
+    return loser * die.turns
+
+
+def solveb(inp):
+    p1, p2 = inp
+
+    pts1 = 0
+    pts2 = 0
+    p1_turn = True
+    count1 = 0
+    count2 = 0
+
+    queue = deque([(pts1, pts2, p1, p2, p1_turn, 1)])
+    step_values = Counter([sum(p) for p in product([1,2,3], [1,2,3], [1,2,3])])
+    threshold = 21
+    while queue:
+        pts1, pts2, p1, p2, p1_turn, factor = queue.popleft()
+        if p1_turn:
+            for steps, count in step_values.items():
+                np1 = do_step(p1, steps)
+                npts1 = pts1 + np1
+                if npts1 >= threshold:
+                    count1 += count * factor
+                else:
+                    queue.append((npts1, pts2, np1, p2, False, count * factor))
+        else:
+            for steps, count in step_values.items():
+                np2 = do_step(p2, steps)
+                npts2 = pts2 + np2
+                if npts2 >= threshold:
+                    count2 += count * factor
+                else:
+                    queue.append((pts1, npts2, p1, np2, True, count * factor))
+        
+    print(f'1: {count1} | 2: {count2}')
+    return max(count1, count2)
+
+
+@cache
+def do_step(p, d):
+    p += d
+    p = (p-1) % 10 + 1
+    return p
+
+
+if __name__=='__main__':
+    example_ans = solveb(example)
+    print(f'example:\n {example_ans}')
+
+    actual_ans = solveb(actual)
+    print(f'actual:\n {actual_ans}')
+
