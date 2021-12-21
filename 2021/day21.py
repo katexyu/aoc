c1fb01fbb5d5dfc64c727c39cef37a82ee0984e4
Kate Yu <katexyu@gmail.com>
2021-12-20 23:20:18 -0800

    Add more optimized version for day 21

--- 2021/day21.py
+++ 2021/day21.py
@@ -46,6 +46,7 @@ def solve(inp):
 
 
 def solveb(inp):
+    # What I actually used to get an answer, takes ~25s
     p1, p2 = inp
 
     pts1 = 0
@@ -87,10 +88,53 @@ def do_step(p, d):
     return p
 
 
+def solveb_better(inp):
+    """
+    8-9x faster than solveb by caching more of the work
+    """
+    p1, p2 = inp
+
+    threshold = 21
+    counts = do_turn(0, 0, p1, p2, True, 1, threshold)
+
+    print(counts)
+    return max(counts)
+
+
+step_counts = Counter([sum(p) for p in product([1,2,3], [1,2,3], [1,2,3])])
+
+
+@cache
+def do_turn(pts1, pts2, p1, p2, p1_turn, factor, threshold):
+    counts = [0, 0]
+    if p1_turn:
+        for steps, count in step_counts.items():
+            np1 = do_step(p1, steps)
+            npts1 = pts1 + np1
+            if npts1 >= threshold:
+                counts[0] += count * factor
+            else:
+                c1, c2 = do_turn(npts1, pts2, np1, p2, False, count * factor, threshold)
+                counts[0] += c1
+                counts[1] += c2
+    else:
+        for steps, count in step_counts.items():
+            np2 = do_step(p2, steps)
+            npts2 = pts2 + np2
+            if npts2 >= threshold:
+                counts[1] += count * factor
+            else:
+                c1, c2 = do_turn(pts1, npts2, p1, np2, True, count * factor, threshold)
+                counts[0] += c1
+                counts[1] += c2
+    return counts
+
+
+
 if __name__=='__main__':
-    example_ans = solveb(example)
+    example_ans = solveb_better(example)
     print(f'example:\n {example_ans}')
 
-    actual_ans = solveb(actual)
+    actual_ans = solveb_better(actual)
     print(f'actual:\n {actual_ans}')
 
