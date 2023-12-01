95cd2ef91f3c46e53d6d7ac64f6bcd20329877f4
Kate Yu <katexyu@gmail.com>
2023-12-01 00:15:16 -0500

    Add 2023 day 1

---
+++ 2023/template.py
@@ -0,0 +1,29 @@
+from helpers import *
+from collections import *
+from itertools import *
+from math import *
+from heapq import heappush, heappop, heappushpop, heapify, heapreplace
+from functools import cache
+
+
+example = """
+"""
+
+actual = """
+"""
+
+
+def solve(inp):
+    inp = inp.strip().split('\n')
+
+    ans = ''
+    return ans 
+
+
+if __name__=='__main__':
+    example_ans = solve(example)
+    print(f'example:\n {example_ans}')
+
+    actual_ans = solve(actual)
+    print(f'actual:\n {actual_ans}')
+
