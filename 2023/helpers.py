8658a916e48395665ae95e0246f7586db9f85d38
Kate Yu <katexyu@gmail.com>
2023-12-10 12:58:40 -0500

    Add 2023 Day 10

--- 2023/helpers.py
+++ 2023/helpers.py
@@ -16,8 +16,8 @@ def print_grid_map(mapping):
     max_y = max(k[1] for k in mapping.keys())
     max_x = max(k[0] for k in mapping.keys())
 
-    for y in range(max_y):
-        print(''.join(mapping[(x, y)] for x in range(max_x)))
+    for y in range(max_y + 1):
+        print(''.join(mapping[(x, y)] for x in range(max_x + 1)))
 
 
 def print_sparse_grid_map(mapping):
